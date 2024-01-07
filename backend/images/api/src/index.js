2c4fdcfd18625082b8c9d4529296357650f19f0c
Stephan Van Hemelrijck <stephan.van.hemelrijck@student.ehb.be>
2024-01-07 08:56:23 +0100

    docs: webpage based documentation on root endpoint

--- backend/images/api/src/index.js
+++ backend/images/api/src/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const ejs = require("ejs");
 
 // Knex
 const knexConfig = require("../knexfile");
@@ -15,6 +16,10 @@ app.use((req, res, next) => {
   next();
 });
 
+// Set EJS as the view engine
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+
 // Routes
 app.use("/api/users", require("./routes/users"));
 app.use("/api/games", require("./routes/games"));
@@ -25,7 +30,13 @@ app.use("/api/games", require("./routes/games"));
  * @returns {string} Hello World
  */
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.render("docs", { title: "GameLibrary - Documentation" });
+});
+
+// Error handler
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something went wrong!");
 });
 
 // Start server
